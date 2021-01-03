84e20a1f4fa62d4ad2e14f3c966caa0bc130eea7
kodcanlisi <developerindiee@gmail.com>
2021-01-03 18:16:53 +0300

    Add constructor and  nthChild function

--- plugin.js
+++ plugin.js
@@ -1,5 +1,20 @@
 class NthChild {
+    constructor(nth){
+      this.nth = nth
+    }
 
+    nthChild(){
+        const  nth  = this.nth;
+        return function ({addVariant }) {
+          addVariant('nth-child', ({ modifySelectors, separator }) => {
+              modifySelectors(({ className }) => {
+                let css = '.nth-child' + "\\"+ separator + className  + `>*:nth-child(${nth})`
+                console.log(nth)
+                return css;
+              });
+          })
+        }
+    }
 }
 
 module.exports = NthChild;
